826a7aa13b65ec06fb72b399930e1eb2d7c512f2
Thomas Berthet <thoberth@student.42.fr>
2023-02-09 09:57:30 +0100

    ex02 finished, use np.log instead of math.log

--- ML03/ex02/vec_log_loss.py
+++ ML03/ex02/vec_log_loss.py
@@ -17,9 +17,10 @@ def vec_log_loss_(y, y_hat, eps=1e-15):
 	Raises:
 	This function should not raise any Exception.
 	"""
-	log_loss = (1/y.shape[0]) @ (y @ math.log(y_hat) +
-                              (np.ones((y.shape[0], 1)) - y) @ math.log(np.ones((y.shape[0], 1)) - y_hat))
-	return log_loss
+	v_ones = np.ones((y.shape[0], 1))
+	log_loss = (y * np.log(y_hat + eps)) + ((v_ones - y)\
+		 * np.log(v_ones - y_hat))
+	return (-1/y.shape[0]) * np.sum(log_loss)
 
 if __name__ == "__main__":
 	y1 = np.array([1]).reshape((-1, 1))
@@ -27,3 +28,15 @@ if __name__ == "__main__":
 	theta1 = np.array([[2], [0.5]])
 	y_hat1 = logistic_predict_(x1, theta1)
 	print(vec_log_loss_(y1, y_hat1))
+
+	y2 = np.array([[1], [0], [1], [0], [1]])
+	x2 = np.array([[4], [7.16], [3.2], [9.37], [0.56]]) 
+	theta2 = np.array([[2], [0.5]])
+	y_hat2 = logistic_predict_(x2, theta2)
+	print(vec_log_loss_(y2, y_hat2))
+
+	y3 = np.array([[0], [1], [1]])
+	x3 = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
+	theta3 = np.array([[-2.4], [-1.5], [0.3], [-1.4], [0.7]])
+	y_hat3 = logistic_predict_(x3, theta3)
+	print(vec_log_loss_(y3, y_hat3))
