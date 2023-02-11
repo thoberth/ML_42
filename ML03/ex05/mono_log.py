90da2f3c7d45b905fa9e14c02072d5f05db217b2
Thomas Berthet <thoberth@student.42.fr>
2023-02-11 20:16:47 +0100

    WIP

--- ML03/ex05/mono_log.py
+++ ML03/ex05/mono_log.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import os, sys
 sys.path.append(os.path.abspath("../ex04/"))
 from my_logistic_regression import MyLogisticRegression as MyLR
@@ -14,6 +15,7 @@ def check_arg(argv):
 	return False
 
 if __name__ == "__main__":
+#### 1 TAKE ZIPCODE AS ARGUMENT BETWEEN 0 - 3
 	if check_arg(sys.argv) == False:
 		exit()
 	try:
@@ -23,8 +25,64 @@ if __name__ == "__main__":
 	except:
 		print('Error with value of "-zipcode" value must be an int between 0 - 3')
 
-#### 1 SPLIT DATASET INTO TRAINING AND TEST SET
-	data = pd.read_csv("solar_system_census.csv")
-	data_planet = pd.read_csv("solar_system_census_planets.csv")
+#### 2 SPLIT DATASET INTO TRAINING AND TEST SET
+#### 3 LABEL EACH CITIZEN: 1=FROM OUR PLANET, 0=ALL OTHERS
+	data = pd.read_csv("resources/solar_system_census.csv")
+	data_planet = pd.read_csv("resources/solar_system_census_planets.csv")
 	# data_planet.drop(data_planet.columns[data_planet.columns.str.contains('Unnamed', case = False)], axis=1, inplace=True)
-	
+	X = np.array(data[['weight', 'height', 'bone_density']])
+	Y = np.array(data_planet[['Origin']])
+	for i in range(len(Y)):
+		if Y[i] == zipcode:
+			Y[i] = 1
+		else:
+			Y[i] = 0
+	x_train, x_test, y_train, y_test = data_spliter(X, Y, 0.5)
+	# print(x_train, x_test, y_train, y_test)
+	mylr = MyLR(np.random.rand(x_train.shape[1] + 1, 1).reshape(-1, 1), alpha=1e-4, max_iter=25000)
+	Y_hat = mylr.predict_(x_train)
+	print('La perte est de {},\nOn entraine notre modele....'.format(mylr.loss_(y_train, Y_hat)))
+#### 4 TRAIN OUR MODEL TO PREDICT ON TRAIN SET
+	mylr.fit_(x_train, y_train)
+	Y_hat = mylr.predict_(x_train)
+	print('La perte est maintenant de {}'.format(
+		mylr.loss_(y_train, Y_hat)))
+#### 5 CALCULATE AND DISPLAY THE FRACTION OF TOTAL PREDICTION
+	y_hat_test = mylr.predict_(x_test)
+	y_hat_test[y_hat_test >= 0.5] = 1
+	y_hat_test[y_hat_test < 0.5] = 0
+	count = 0
+	for i in range(len(y_hat_test)):
+		if y_hat_test[i] == y_test[i]:
+			count += 1
+	print('On predit avec les test set et on obtient:', (count*100)/len(y_hat_test), '% de valeur exacte')
+#### 6 plot 3 scatter
+	plt.figure()
+	plt.scatter(x_test[:, 0], y_hat_test,
+	            label='prediction', c='#51348a', alpha=0.5)
+	plt.scatter(x_test[:, 0], y_test, label='vraie valeur',
+	            c='#32a852', alpha=0.5)
+	plt.xlabel('Weight')
+	plt.ylabel('1 = is from our Planet,\n0 = is not from our Planet')
+	plt.legend()
+	plt.show()
+
+	plt.figure()
+	plt.scatter(x_test[:, 1], y_hat_test,
+	            label='prediction', c='#51348a', alpha=0.5)
+	plt.scatter(x_test[:, 1], y_test, label='vraie valeur',
+	            c='#32a852', alpha=0.5)
+	plt.legend()
+	plt.xlabel('Height')
+	plt.ylabel('1 = is from our Planet,\n0 = is not from our Planet')
+	plt.show()
+
+	plt.figure()
+	plt.scatter(x_test[:, 2], y_hat_test,
+	            label='prediction', c='#51348a', alpha=0.5)
+	plt.scatter(x_test[:, 2], y_test, label='vraie valeur',
+	            c='#32a852', alpha=0.5)
+	plt.xlabel('Bone Density')
+	plt.ylabel('1 = is from our Planet,\n0 = is not from our Planet')
+	plt.legend()
+	plt.show()
