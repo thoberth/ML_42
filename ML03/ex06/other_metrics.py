6f7d6098bbcbdb39c02be8c4d83d97b066507803
Thomas Berthet <thoberth@student.42.fr>
2023-02-13 19:53:17 +0100

    ex06 finish and add test

--- ML03/ex06/other_metrics.py
+++ ML03/ex06/other_metrics.py
@@ -17,7 +17,7 @@ def count_metrics(y, y_hat, pos_label=1):
 				fn += 1
 	return tp, tn, fp, fn
 
-def accuracy_score_(y, y_hat):
+def accuracy_score_(y, y_hat, pos_label=1):
 	"""
 	Compute the accuracy score.
 	Args:
@@ -29,7 +29,7 @@ def accuracy_score_(y, y_hat):
 	Raises:
 		This function should not raise any Exception.
 	"""
-	tp, tn, fp, fn = count_metrics(y, y_hat)
+	tp, tn, fp, fn = count_metrics(y, y_hat, pos_label)
 	return (tp + tn) / (tp + fp + tn + fn)
 
 def precision_score_(y, y_hat, pos_label=1):
@@ -45,7 +45,7 @@ def precision_score_(y, y_hat, pos_label=1):
 	Raises:
 		This function should not raise any Exception.
 	"""
-	tp, tn, fp, fn = count_metrics(y, y_hat)
+	tp, tn, fp, fn = count_metrics(y, y_hat, pos_label)
 	return (tp / (tp + fp))
 
 
@@ -62,7 +62,7 @@ def recall_score_(y, y_hat, pos_label=1):
 	Raises:
 		This function should not raise any Exception.
 	"""
-	tp, tn, fp, fn = count_metrics(y, y_hat)
+	tp, tn, fp, fn = count_metrics(y, y_hat, pos_label)
 	return (tp / (tp + fn))
 
 def f1_score_(y, y_hat, pos_label=1):
@@ -78,8 +78,8 @@ def f1_score_(y, y_hat, pos_label=1):
 	Raises:
 		This function should not raise any Exception.
 	"""
-	prec = precision_score_(y, y_hat)
-	rec = recall_score_(y, y_hat)
+	prec = precision_score_(y, y_hat, pos_label)
+	rec = recall_score_(y, y_hat, pos_label)
 	return ((2 * prec * rec)/(prec + rec))
 
 if __name__ == "__main__":
@@ -114,3 +114,60 @@ if __name__ == "__main__":
 	# sklearn implementation
 	print(f1_score(y, y_hat))
 	# Output: 0.5
+
+	# Example 2:
+	y_hat = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+	y = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+	# Accuracy
+	# your implementation 
+	print(accuracy_score_(y, y_hat))
+	## Output: 0.625
+	# sklearn implementation 
+	print(accuracy_score(y, y_hat))
+	## Output: 0.625
+	# Precision
+	# your implementation
+	print(precision_score_(y, y_hat, pos_label='dog'))
+	# Output: 0.6
+	# sklearn implementation 
+	print(precision_score(y, y_hat, pos_label='dog'))
+	## Output: 0.6
+	# Recall
+	# your implementation
+	print(recall_score_(y, y_hat, pos_label='dog'))
+	# Output: 0.75
+	# sklearn implementation 
+	print(recall_score(y, y_hat, pos_label='dog'))
+	## Output:0.75
+	# F1-score
+	# your implementation
+	print(f1_score_(y, y_hat, pos_label='dog'))
+	# Output: 0.6666666666666665
+	# sklearn implementation
+	print(f1_score(y, y_hat, pos_label='dog'))
+	## Output: 0.6666666666666665
+
+	# Example 3:
+	y_hat = np.array(['norminet', 'dog', 'norminet', 'norminet', 'dog', 'dog', 'dog', 'dog'])
+	y = np.array(['dog', 'dog', 'norminet', 'norminet', 'dog', 'norminet', 'dog', 'norminet'])
+	# Precision
+	# your implementation
+	print(precision_score_(y, y_hat, pos_label='norminet'))
+	# Output: 0.6666666666666666
+	# sklearn implementation
+	print(precision_score(y, y_hat, pos_label='norminet'))
+	# Output: 0.6666666666666666
+	# Recall
+	# your implementation
+	print(recall_score_(y, y_hat, pos_label='norminet'))
+	# Output:0.5
+	# sklearn implementation
+	print(recall_score(y, y_hat, pos_label='norminet'))
+	# Output:0.5
+	# F1-score
+	# your implementation
+	print(f1_score_(y, y_hat, pos_label='norminet'))
+	# Output:0.5714285714285715
+	# sklearn implementation
+	print(f1_score(y, y_hat, pos_label='norminet'))
+	# Output:0.5714285714285715
