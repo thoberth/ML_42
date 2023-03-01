1c313020f6d25dc44be6a10f8123a75e4c839383
Thomas Berthet <thoberth@student.42.fr>
2023-03-01 15:30:32 +0100

    wip

--- ML04/ex08/solar_system_census.py
+++ ML04/ex08/solar_system_census.py
@@ -1,10 +1,29 @@
-import os, sys
-import numpy as np
-import pandas as pd
-sys.path.append(os.path.abspath('../ex00/'))
-sys.path.append(os.path.abspath('../ex07'))
+from benchmark_train import *
 
 if __name__ == "__main__":
 	df_x = pd.read_csv('resources/solar_system_census.csv')
 	df_y = pd.read_csv('resources/solar_system_census_planets.csv')
+	X = np.array(df_x[['weight', 'height', 'bone_density']])
+	y = np.array(df_y[['Origin']])
+
+	X = add_polynomial_features(X, 3)
+	for i in range(X.shape[1]):
+		X[:, i] = zscore(X[:, i])
+
+	file = open("models.pickle", "rb") # "rb" -> read in binary mode
+	models = pickle.load(file)
+	file.close()
+
+	best = None
+	index = 0
+	for i, (key, value) in enumerate(models['score'].items()):
+		if best == None:
+			best = {key: value}
+		elif list(best.values()) < value:
+			best = {key: value}
+			index = i
+	# print('best is ', best, index)
+	res = str(list(best.keys())[0])
+	w, h, b, lambda_ = int(res[1]), int(res[3]), int(res[5]), float(res[7:])
+	to_train = X[:, np.concatenate(([0, 3, 6][:w], [1, 4, 7][:h], [2, 5, 8][:b]))]
 	
