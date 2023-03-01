e41c37a8e7d687f3418caab9be803247c0a7d417
Thomas BERTHET <thoberth@students.42.fr>
2023-03-01 18:31:23 +0100

    WIP

--- ML04/ex08/solar_system_census.py
+++ ML04/ex08/solar_system_census.py
@@ -1,5 +1,12 @@
 from benchmark_train import *
 
+def train_models(X, y, lambda_):
+	lr = MyLR(theta=np.random.rand(
+				X.shape[1] + 1, 1).reshape(-1, 1), max_iter=15000, lambda_=lambda_)
+	print(lr.loss_(lr.predict_(X), y))
+	lr.fit_(X, y)
+	print(lr.loss_(lr.predict_(X), y))
+
 if __name__ == "__main__":
 	df_x = pd.read_csv('resources/solar_system_census.csv')
 	df_y = pd.read_csv('resources/solar_system_census_planets.csv')
@@ -9,6 +16,8 @@ if __name__ == "__main__":
 	X = add_polynomial_features(X, 3)
 	for i in range(X.shape[1]):
 		X[:, i] = zscore(X[:, i])
+
+	x_train, x_test, y_train, y_test = data_spliter(X, y, 0.7)
 
 	file = open("models.pickle", "rb") # "rb" -> read in binary mode
 	models = pickle.load(file)
@@ -22,8 +31,8 @@ if __name__ == "__main__":
 		elif list(best.values()) < value:
 			best = {key: value}
 			index = i
-	# print('best is ', best, index)
+	print('best is ', best, index)
 	res = str(list(best.keys())[0])
 	w, h, b, lambda_ = int(res[1]), int(res[3]), int(res[5]), float(res[7:])
-	to_train = X[:, np.concatenate(([0, 3, 6][:w], [1, 4, 7][:h], [2, 5, 8][:b]))]
-	
+	to_train = x_train[:, np.concatenate(([0, 3, 6][:w], [1, 4, 7][:h], [2, 5, 8][:b]))]
+	train_models(to_train, y_train, lambda_)
